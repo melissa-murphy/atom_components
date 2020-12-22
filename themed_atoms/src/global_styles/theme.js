42c07c74394be1bb3087877c6c0b23db320c8bea
Melissa Murphy <melissa@thoughtfulmonkey.design>
2020-12-21 22:58:13 -0800

    started new base app created basic themeing for wrapper

---
+++ themed_atoms/src/global_styles/theme.js
@@ -0,0 +1,30 @@
+// all items can be changed to create new global theming
+// new categories can be  added and incorporated into components (TODO: instructions)
+
+const theme = {
+  color: {
+    primary: 'blue',
+    secondary: 'grey',
+    tertiary: 'green',
+    dark: 'black',
+    light: 'white',
+    display: 'cyan',
+    text: 'grey',
+    stroke: 'black',
+  },
+  // fonts: {}
+  fontSize: {
+    sm: '0.5em',
+    reg: '1.25em',
+    lg: '1.75em',
+    display: '2.5em',
+  },
+  breakpoint: {
+    mobile: '576px',
+    tablet: '768px',
+    laptop: '1024px',
+    lg: '1400',
+  },
+};
+
+export default theme;
