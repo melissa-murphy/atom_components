9f2fa2b256a9b9ebc5bf3e80a96a5b129f556dc1
Melissa Mondot <31637708+melissa-mondot@users.noreply.github.com>
2021-07-13 20:09:24 -0700

    Add theme context

---
+++ themed/src/contexts/ThemeStore.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+// Create theme context/state with toggle
+
+const ThemeContext = React.createContext();
+
+const ThemeStore = ({ children }) => {
+  // default state
+  const [theme, setTheme] = useState("light");
+  //   switch state
+  const toggleTheme = (theme) => setTheme(theme);
+
+  return (
+    <ThemeContext.Provider value={{ toggleTheme, theme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeStore, ThemeContext };
